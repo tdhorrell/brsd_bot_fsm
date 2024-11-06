3e9805dfc26a97796b728741e8811b76d55b51b8
Tim <td.horrell11@gmail.com>
2024-11-06 14:57:07 -0500

    Full FSM v1

--- FSM_bot.py
+++ FSM_bot.py
@@ -64,7 +64,7 @@ class BRSD_sm(object):
         return (self.theta > 20 and self.dist > 0)
 
     def r1_to_f1(self, even_data):
-        return (self.theta >= 5 and self.dist > 0)
+        return (self.theta <= 5 and self.dist > 0)
     
 
     # right 2 transitions
@@ -74,10 +74,10 @@ class BRSD_sm(object):
     # stop, backwards transitions can be handled by existing checks above
     # here are transitions INTO the stop states
     def state_to_s1(self, even_data):
-        return (self.bump_flag == True)
+        return self.bump_flag == True
     
     def state_to_s2(self, even_data):
-        return (self.user_flag == False)
+        return self.user_flag == False
 
 # define machine states
 states = ['i1', 'f1', 'f2', 'l1', 'l2', 'r1', 'r2', 's1', 's2', 'b1']
@@ -85,47 +85,47 @@ states = ['i1', 'f1', 'f2', 'l1', 'l2', 'r1', 'r2', 's1', 's2', 'b1']
 # define transition conditions
 transitions = [
     # idle 1 transitions
+    {'trigger': 'check', 'source': 'i1', 'dest': 's1', 'conditions': 'state_to_s1'},
+    {'trigger': 'check', 'source': 'i1', 'dest': 's2', 'conditions': 'state_to_s2'},
     {'trigger': 'check', 'source': 'i1', 'dest': 'f1', 'conditions': 'i1_to_f1'},
     {'trigger': 'check', 'source': 'i1', 'dest': 'l1', 'conditions': 'i1_to_l1'},
     {'trigger': 'check', 'source': 'i1', 'dest': 'r1', 'conditions': 'i1_to_r1'},
-    {'trigger': 'check', 'source': 'i1', 'dest': 's1', 'conditions': 'state_to_s1'},
-    {'trigger': 'check', 'source': 'i1', 'dest': 's2', 'conditions': 'state_to_s2'},
     
     # forward 1 transitions
+    {'trigger': 'check', 'source': 'f1', 'dest': 's1', 'conditions': 'state_to_s1'},
+    {'trigger': 'check', 'source': 'f1', 'dest': 's2', 'conditions': 'state_to_s2'},
     {'trigger': 'check', 'source': 'f1', 'dest': 'l1', 'conditions': 'f1_to_l1'},
     {'trigger': 'check', 'source': 'f1', 'dest': 'r1', 'conditions': 'f1_to_r1'},
     {'trigger': 'check', 'source': 'f1', 'dest': 'f2', 'conditions': 'f1_to_f2'},   
-    {'trigger': 'check', 'source': 'f1', 'dest': 's1', 'conditions': 'state_to_s1'},
-    {'trigger': 'check', 'source': 'f1', 'dest': 's2', 'conditions': 'state_to_s2'},
 
     # forward 2 transitions (can use same transitions as f1)
+    {'trigger': 'check', 'source': 'f2', 'dest': 's1', 'conditions': 'state_to_s1'},
+    {'trigger': 'check', 'source': 'f2', 'dest': 's2', 'conditions': 'state_to_s2'},
     {'trigger': 'check', 'source': 'f2', 'dest': 'l1', 'conditions': 'f1_to_l1'},
     {'trigger': 'check', 'source': 'f2', 'dest': 'r1', 'conditions': 'f1_to_r1'},
     # {'trigger': 'check', 'source': 'f2', 'dest': 'f1', 'conditions': 'f2_to_f1'}, 
-    {'trigger': 'check', 'source': 'f2', 'dest': 's1', 'conditions': 'state_to_s1'},
-    {'trigger': 'check', 'source': 'f2', 'dest': 's2', 'conditions': 'state_to_s2'},
 
     # left 1 transitions
-    {'trigger': 'check', 'source': 'l1', 'dest': 'f1', 'conditions': 'l1_to_f1'},
-    {'trigger': 'check', 'source': 'l1', 'dest': 'l2', 'conditions': 'l1_to_l2'},
     {'trigger': 'check', 'source': 'l1', 'dest': 's1', 'conditions': 'state_to_s1'},
     {'trigger': 'check', 'source': 'l1', 'dest': 's2', 'conditions': 'state_to_s2'},
+    {'trigger': 'check', 'source': 'l1', 'dest': 'f1', 'conditions': 'l1_to_f1'},
+    {'trigger': 'check', 'source': 'l1', 'dest': 'l2', 'conditions': 'l1_to_l2'},
 
     # left 2 transitions
-    {'trigger': 'check', 'source': 'l2', 'dest': 'l1', 'conditions': 'l2_to_l1'},
     {'trigger': 'check', 'source': 'l2', 'dest': 's1', 'conditions': 'state_to_s1'},
     {'trigger': 'check', 'source': 'l2', 'dest': 's2', 'conditions': 'state_to_s2'},
+    {'trigger': 'check', 'source': 'l2', 'dest': 'l1', 'conditions': 'l2_to_l1'},
 
     # right 1 transitions
-    {'trigger': 'check', 'source': 'r1', 'dest': 'f1', 'conditions': 'r1_to_f1'},
-    {'trigger': 'check', 'source': 'r1', 'dest': 'r2', 'conditions': 'r1_to_r2'},
     {'trigger': 'check', 'source': 'r1', 'dest': 's1', 'conditions': 'state_to_s1'},
     {'trigger': 'check', 'source': 'r1', 'dest': 's2', 'conditions': 'state_to_s2'},
+    {'trigger': 'check', 'source': 'r1', 'dest': 'f1', 'conditions': 'r1_to_f1'},
+    {'trigger': 'check', 'source': 'r1', 'dest': 'r2', 'conditions': 'r1_to_r2'},
 
     # right 2 transitions
-    {'trigger': 'check', 'source': 'r2', 'dest': 'r1', 'conditions': 'r2_to_r1'},
     {'trigger': 'check', 'source': 'r2', 'dest': 's1', 'conditions': 'state_to_s1'},
     {'trigger': 'check', 'source': 'r2', 'dest': 's2', 'conditions': 'state_to_s2'},
+    {'trigger': 'check', 'source': 'r2', 'dest': 'r1', 'conditions': 'r2_to_r1'},
 
     # stop 1 transitions  
     {'trigger': 'check', 'source': 's1', 'dest': 'b1', 'conditions': 'state_pass'},
@@ -136,11 +136,11 @@ transitions = [
     {'trigger': 'check', 'source': 's2', 'dest': 'f1', 'conditions': 'i1_to_f1'},
 
     # backwards 1 transitions
+    {'trigger': 'check', 'source': 'b1', 'dest': 's1', 'conditions': 'state_to_s1'},
+    {'trigger': 'check', 'source': 'b1', 'dest': 's2', 'conditions': 'state_to_s2'},    
     {'trigger': 'check', 'source': 'b1', 'dest': 'l1', 'conditions': 'f1_to_l1'},
     {'trigger': 'check', 'source': 'b1', 'dest': 'r1', 'conditions': 'f1_to_r1'},
-    {'trigger': 'check', 'source': 'b1', 'dest': 'f1', 'conditions': 'i1_to_f1'},
-    {'trigger': 'check', 'source': 'b1', 'dest': 's1', 'conditions': 'state_to_s1'},
-    {'trigger': 'check', 'source': 'b1', 'dest': 's2', 'conditions': 'state_to_s2'},]
+    {'trigger': 'check', 'source': 'b1', 'dest': 'f1', 'conditions': 'i1_to_f1'}]
 
 class BRSD_machine(object):
 
@@ -158,18 +158,29 @@ class BRSD_machine(object):
     def run(self):
         while self.running:
             try:
+                # track current state data
                 self.state_mem.append(self.model.state)
                 print('At state ' + self.model.state)
                 print(f'\tX: ',self.model.toy_array_x[self.model.point_index],'\t\tY: ',self.model.toy_array_y[self.model.point_index])
                 print(f'\tDist: ',self.model.dist,'\tTheta: ',self.model.theta)
-                while not self.model.check():
-                    sleep(1)
-                    self.model.point_index += 1
-                    self.model.calc_theta()
+
+                # call the transition change
+                self.model.point_index += 1
+                self.model.calc_theta()
+                self.model.check()
+                sleep(1)
+
+                # check stop flags
+                if self.model.point_index == 4:
+                    self.model.user_flag = False
+                
+                if self.model.point_index == 5:
+                    self.model.user_flag = True
+
             except IndexError:
-                print('\n*****\nEnd of Test Reached\n*****')
+                print('\n*****\nEnd of Test Reached\n*****\n')
                 break
             
 brsd = BRSD_machine()
 brsd.run()
-print(brsd.state_mem)
+print(f'State Memory: ',brsd.state_mem)
